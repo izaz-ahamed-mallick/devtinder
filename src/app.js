a59544e0efe8d9356082fe35361b86a557b37782
Izaz Ahamed Mallick <izazahamedmallick98@gmail.com>
2026-09-11 23:23:32 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -1,12 +1,46 @@
 const express = require("express")
 const app = express()
 
-app.use((req,res)=>{
-  res.send("Hello Dada")
+app.get("/user",(req,res)=>{
+  res.send({name:"Tanish",address:"ujjalpukur"})
 })
+
+
+app.use(express.json());
+
+app.post("/user", (req, res) => {
+  console.log(req.body);
+  res.json({
+    message: "User saved successfully on DB",
+    user: req.body
+  });
+});
+
+app.put("/user",(req,res)=>{
+  
+  res.send("User update successfully " + req.query.name)
+})
+
+app.delete("/user/:id",(req,res)=>{
+  res.send(`User delete successfully - ${req.params.id}`)
+})
+
+app.use("/",(req,res,next)=>{
+ console.log("Home middleware");
+  next()
+})
+app.use("/hey/2",(req,res)=>{
+  res.send("Hey Dada 2")
+})
+
 app.use("/hey",(req,res)=>{
   res.send("Hey Dada")
 })
+
+app.use("/user/:name",(req,res)=>{
+  res.send(`Hello-${req.params.name}`)
+})
+
 
 app.listen(3000,()=>{
   console.log("Server start successfully!")
